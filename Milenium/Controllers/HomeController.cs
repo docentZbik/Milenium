a68ad07b1584a3b75558f72cbab234d36ef925e3
Maciej Napiórkowski <maciej500@gmail.com>
2021-04-20 18:56:16 +0200

    Removed unused code and comments

--- Milenium/Controllers/HomeController.cs
+++ Milenium/Controllers/HomeController.cs
@@ -34,33 +34,5 @@ namespace Milenium.Controllers
         {
             return View("Summary",userData);
         }
-
-
-        private bool ValidateCultureType(string cultureType)
-        {
-            try
-            {
-                CultureInfo.GetCultureInfo(cultureType);
-                return true;
-            }
-            catch(CultureNotFoundException)
-            {
-                return false;
-            }
-        }
-
-        private void ChangeCurrentCulutre(string cultureType)
-        {
-            var currentCulture = CultureInfo.CurrentCulture.Name;
-
-            if(currentCulture != cultureType)
-            {
-                var targetCultureInfo = new CultureInfo(cultureType);
-                CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureType);
-                CultureInfo.CurrentUICulture = CultureInfo.CreateSpecificCulture(cultureType);
-                CultureInfo.DefaultThreadCurrentCulture = targetCultureInfo;
-                CultureInfo.DefaultThreadCurrentUICulture = targetCultureInfo;
-            }
-        }
     }
 }
--- Milenium/Models/UserData.cs
+++ Milenium/Models/UserData.cs
@@ -8,13 +8,11 @@ namespace Milenium.Models
 {
     public class UserData
     {
-        //[Required(ErrorMessageResourceName = "nicknameErrorMessage", ErrorMessageResourceType = typeof(Resource))]
         [Display(Name ="nickname", ResourceType = typeof(Resource))]
         [RegularExpression(@"^[a-zA-Z]+$",ErrorMessageResourceName = "nicknameErrorMessage", ErrorMessageResourceType = typeof(Resource))]
         [MaxLength(30)]
         public string Nickname { get; set; }
 
-        //[Required(ErrorMessageResourceName ="emailErrorMessage", ErrorMessageResourceType = typeof(Resource))]
         [Display(Name = "email", ResourceType = typeof(Resource))]
         [EmailAddress(ErrorMessageResourceName ="emailErrorMessage", ErrorMessageResourceType = typeof(Resource))]
         public string EmailAdress { get; set; }
